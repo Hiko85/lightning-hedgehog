d696ea444b2966134e0606e961dbc093e20ba9db
adrirsz <adriennorosz@pm.me>
2024-12-12 19:28:49 +0100

    Hörcsögválasztás (hamster_logic)

--- hamster/hamster_logic.py
+++ hamster/hamster_logic.py
@@ -3,5 +3,12 @@ def hamster_welcome():
     print("Start")
 
 def hamster_name():
-    horcsog_nev = input("Először is nevezd el a hörcsögöt: ")
-    horcsog_fajta = input(f"Milyen fajta hörcsög {horcsog_nev}? \nAdd meg a hörcsögfajtához tatozó számot: \n1 - Törpe Roborovski \n2 - Campbell törpe orosz \n3 - Szíriai (arany) hörcsög\n")
+    hamster_name = input("Először is nevezd el a hörcsögöt: ")
+    hamster_type = int(input(f"Milyen fajta hörcsög {hamster_name}? \nAdd meg a hörcsögfajtához tatozó számot: \n1 - Törpe Roborovski \n2 - Campbell törpe orosz \n3 - Szíriai (arany) hörcsög\n"))
+    
+    if hamster_type == 1 :
+        print(f"{hamster_name} egy csodálatos Törpe Roborovski!")
+    elif hamster_type == 2 :
+        print(f"{hamster_name} egy utánozhatatlan Campbell törpe orosz!")
+    elif hamster_type == 3 :
+        print(f"{hamster_name} egy felejthetetlen Szíriai (arany) hörcsög!")

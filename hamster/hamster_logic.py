c6ae39d87dd2ca866a43217316d8249797ab1ae4
adrirsz <132218179+adrirsz@users.noreply.github.com>
2025-01-03 10:57:33 +0100

    Hörcsögetetés update #1

--- hamster/hamster_logic.py
+++ hamster/hamster_logic.py
@@ -1,8 +1,11 @@
 def hamster_welcome():
-    input("Üdv a hörcsög tamagotchi játékban! \nA feladatod, hogy gondokodj egy hörcsögről. Nyomj egy ENTERt, ha kezdhetjük!")
+    input("Üdv a hörcsög tamagotchi játékban! \nA feladatod, hogy gondoskodj egy hörcsögről. Nyomj egy ENTERt, ha kezdhetjük! ")
     print("Start")
 
+hamster_name = "" # globális változó
+
 def hamster_name():
+    global hamster_name
     hamster_name = input("Először is nevezd el a hörcsögöt: ")
     hamster_type = int(input(f"Milyen fajta hörcsög {hamster_name}? \nAdd meg a hörcsögfajtához tatozó számot: \n1 - Törpe Roborovski \n2 - Campbell törpe orosz \n3 - Szíriai (arany) hörcsög\n"))
     
@@ -14,14 +17,26 @@ def hamster_name():
         print(f"{hamster_name} egy felejthetetlen Szíriai (arany) hörcsög!")
 
 def hamster_food():
-    print("\nA hörcsögöd éhes! Mit adsz neki enni?")
+    global hamster_name
+    print(f"\n{hamster_name}, a hörcsögöd éhes! Mit adsz neki enni?")
     print("A) Zöldségeket")
     print("B) Csipszet")
-    answer = input("Válaszd A vagy B: ")
 
-    if answer.lower() == "a":
-        print("A hörcsögöd nagyon örül a friss zöldségeknek! Jól választottál.")
-    elif answer.lower() == "b":
-        print("A hörcsögöd nem igazán szereti a csipszet, de nem bánja annyira.")
-    else:
-        print("Érvénytelen válasz, próbáld újra!")
+    hamster_fullness = 0
+
+    while hamster_fullness < 10 :
+        answer = input("Válassz, A vagy B: ")
+        if hamster_fullness == 10 :
+            print(f"{hamster_name} jól lakott, köszöni szépen!")
+        elif answer.lower() == "a":
+            print(f"{hamster_name} nagyon örül a friss zöldségeknek! Jól választottál.")
+            hamster_fullness += 1
+            print(hamster_fullness)
+        elif answer.lower() == "b":
+            print(f"{hamster_name} nem igazán szereti a csipszet, de nem bánja annyira.")
+            hamster_fullness -= 1
+            print(hamster_fullness)
+        else:
+            print("Érvénytelen válasz, próbáld újra!")
+
+

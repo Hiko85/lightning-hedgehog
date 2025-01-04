a6f6f2ec361dd27b54377d518351fe281c7e1ab8
Hiko85 <oszi.ildiko85@gmail.com>
2025-01-04 02:57:37 +0100

    detective_main.py kialakítása, az alfájlok importálása, kezdeti felépítése

--- detective/detective_main.py
+++ detective/detective_main.py
@@ -1,7 +1,32 @@
 #főoldal ami kapcsolatba lépjen a felhasználóval
-import detective_user
+from detective_user import *
+from detective_state import *
+from detective_logic import *
 
-def welcome():
-    
-    print("\nAlapsztori: Öt évvel ezelőtt, október 1-én az elsőéves Kamonohashi Ront választották ki egy gyakorlatra, hogy a rendőrséggel együtt dolgozzon egy hét bérgyilkosból álló csoport elfogásán. Ron megfejtette a bűnözők által használt kódot, és megtalálta a rejtekhelyüket egy elszigetelt gyárban. Délután négykor egyedül indult oda. Amikor a rendőrség egy órával később megérkezett, Ront a gyilkosok holttestei között találták, sérülésekkel borítva és egy késsel a kezében. Ron nem emlékszik az eseményekre, és az eset rejtély maradt.")
+def main():
+#a játék fő funkciói.
 
+    role = welcome()
+    if not role:
+        return
+
+    state = GameState()
+
+    if role == "detective":
+        detective_story(state)
+    elif role == "killer":
+        killer_story(state)
+
+    print("\nA játék véget ért. Az elért pontszámod:", state.get_score())
+    if role == "detective":
+        if state.get_score() >= 50:
+            print("Gratulálunk! Sikerült elkapnod a gyilkost!")
+        else:
+            print("Sajnáljuk, nem sikerült elkapnod a gyilkost.")
+    elif role == "killer":
+        if state.get_score() >= 50:
+            print("Gratulálunk! Sikerült megúsznod a bűntényt!")
+        else:
+            print("Elkaptak a rendőrök. A bűntett kiderült.")
+
+main()

6d74f104c61eaa2f3a0be7722fb56eb23b56e2c2
Marcella Markovits <marcella_marko@yahoo.com>
2025-01-08 15:28:00 +0100

    importok  tic tac to py ba

---
+++ tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,4 @@
+from tic_tac_toe_logic import check_winner, is_board_full, starting
+from tic_tac_toe_user import get_player_move, display_winner, display_draw, welcome
+# Importáljuk a globális változókat és a print_board függvényt
+from tic_tac_toe_state import initialize_game_state, print_board
